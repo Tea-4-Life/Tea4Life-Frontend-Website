b1cf9d53f38439247bcb71af67ef93f9e2fed8c4
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-09 14:54:30 +0700

    feat: Add product query types and API service functions.

--- src/services/productApi.ts
+++ src/services/productApi.ts
@@ -1,18 +1,26 @@
 import axiosClient from "@/lib/axios-client";
 import type ApiResponse from "@/types/base/ApiResponse";
 import type PageResponse from "@/types/base/PageResponse";
-import type PaginationParams from "@/types/base/PaginationParams";
-import type { ProductResponse } from "@/types/product/ProductResponse";
+import type { ProductDetailResponse } from "@/types/product/ProductDetailResponse";
 import type { ProductSummaryResponse } from "@/types/product/ProductSummaryResponse";
 
-export const getProductsApi = async (params: PaginationParams) => {
+import type { ProductQuery } from "@/types/product/ProductQuery";
+import type { ProductCategoryResponse } from "@/types/product-category/ProductCategoryResponse";
+
+export const getProductsApi = async (params: ProductQuery) => {
   return await axiosClient.get<
     ApiResponse<PageResponse<ProductSummaryResponse>>
   >("/product-service/products", { params });
 };
 
+export const getProductCategoriesApi = async () => {
+  return await axiosClient.get<ApiResponse<ProductCategoryResponse[]>>(
+    "/product-service/products/categories",
+  );
+};
+
 export const getProductByIdApi = async (id: string) => {
-  return await axiosClient.get<ApiResponse<ProductResponse>>(
+  return await axiosClient.get<ApiResponse<ProductDetailResponse>>(
     `/product-service/products/${id}`,
   );
 };
--- src/types/product-option/ProductOptionResponse.d.ts
+++ src/types/product-option/ProductOptionResponse.d.ts
@@ -1,8 +1,10 @@
+import type { ProductOptionValueResponse } from "./ProductOptionValueResponse";
+
 export interface ProductOptionResponse {
   id: string;
   name: string;
   isRequired: boolean;
   isMultiSelect: boolean;
   sortOrder: number;
-  productIds: string[];
+  productOptionValues: ProductOptionValueResponse[];
 }
